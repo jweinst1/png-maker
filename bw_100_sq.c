8eaad8a6ffe9bb915b40b5f1aa5794c662a5da69
Joshua Weinstein <jweinstein@splunk.com>
2020-09-01 13:52:15 -0700

    region fill

--- bw_100_sq.c
+++ bw_100_sq.c
@@ -12,6 +12,8 @@
 
 static unsigned char CANVAS[CANVAS_SIZE] = {COLOR_BLACK};
 
+#define CANVAS_AT(x, y) (CANVAS[(CANVAS_Y * y) + x])
+
 static void draw_ly(size_t y, unsigned char gc)
 {
    size_t i = y * CANVAS_Y;
@@ -31,15 +33,28 @@ static void draw_lx(size_t x, unsigned char gc)
    }
 }
 
+static void draw_rg(size_t x1, size_t x2, size_t y1, size_t y2, unsigned char gc)
+{
+    size_t x;
+    size_t y;
+    for (x = x1; x < x2 ; x++) {
+        for (y = y1; y < y2 ; y++) {
+            CANVAS_AT(x, y) = gc;
+        }
+    } 
+}
+
 int  main(int argc, char const* argv[])
 {
     draw_ly(10, COLOR_WHITE);
     draw_lx(10, COLOR_WHITE);
     draw_ly(50, 127);
     draw_lx(30, 127);
+    draw_rg(10, 30, 10, 30, COLOR_WHITE);
     fprintf(stdout, "Writing png to %s\n", CANVAS_FILE);
     return write_png_gray(CANVAS_FILE, 100, 100, 8, CANVAS, "bw100");
 }
 
 #undef CANVAS_SIZE
+#undef CANVAS_AT
 
